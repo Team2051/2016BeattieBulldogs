ff10cf1981c26ef639d9c77e35c4096869b49f0b
Kaamin <ClarkJackson9@gmail.com>
2016-01-27 17:49:36 -0500

    Commit 1/27/2016

---
+++ Stronghold/src/org/usfirst/frc/team2051/robot/RobotMap.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2051.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap 
+{
+    // For example to map the left and right motors, you could define the
+    // following variables to use with your drivetrain subsystem.
+    // public static int leftMotor = 1;
+    // public static int rightMotor = 2;
+	public static final int DRIVE_LEFT_A_CAN_ID = 1;
+	public static final int DRIVE_LEFT_B_CAN_ID = 2;
+	public static final int DRIVE_LEFT_C_CAN_ID = 3;
+
+	public static final int DRIVE_RIGHT_A_CAN_ID = 4;
+	public static final int DRIVE_RIGHT_B_CAN_ID = 5;
+	public static final int DRIVE_RIGHT_C_CAN_ID = 6;
+	
+    // If you are using multiple modules, make sure to define both the port
+    // number and the module. For example you with a rangefinder:
+    // public static int rangefinderPort = 1;
+    // public static int rangefinderModule = 1;
+}
---
+++ Stronghold/src/org/usfirst/frc/team2051/robot/commands/DriveByJoystick.java
@@ -0,0 +1,50 @@
+
+package org.usfirst.frc.team2051.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import org.usfirst.frc.team2051.robot.Robot;
+
+/**
+ *
+ */
+public class DriveByJoystick extends Command 
+{
+
+	    public DriveByJoystick() 
+	    {
+	        // Use requires() here to declare subsystem dependencies
+	        requires(Robot.driveSystem);
+	    }
+
+	    // Called just before this Command runs the first time
+	    protected void initialize() 
+	    {
+	    }
+
+	    // Called repeatedly when this Command is scheduled to run
+	    protected void execute() 
+	    {
+			Robot.driveSystem.takeJoystickInputs(Robot.oi.getDriveStick());
+	    }
+
+	    // Make this return true when this Command no longer needs to run execute()
+	    protected boolean isFinished() 
+	    {
+	        return false;
+	    }
+
+	    // Called once after isFinished returns true
+	    protected void end() 
+	    {
+			Robot.driveSystem.stop();
+	    }
+
+	    // Called when another command which requires one or more of the same
+	    // subsystems is scheduled to run
+	    protected void interrupted() 
+	    {
+	    	end();
+	    }
+}
